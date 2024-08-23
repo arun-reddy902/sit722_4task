98cc8c460fd2a6a1aa4a2fedd50f309aa458b1c7
arun-reddy902 <109848195+arun-reddy902@users.noreply.github.com>
2024-08-24 02:45:01 +1000

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = os.getenv("postgresql://arunreddy_902_user:9cu30xeoee9L9eyaDw6Y34rnntf2K0Mm@dpg-cr3nfbd6l47c73a8is0g-a.oregon-postgres.render.com/arunreddy_902")
 
 settings = Settings()
